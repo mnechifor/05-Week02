be0664af2b2f0e19f5451fe0f074a082b22cf2f9
Mihai Nechifor <Mihai.Nechifor@endava.com>
2019-10-15 20:57:08 +0300

    2

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -11,8 +11,58 @@ namespace Week02
         static void Main(string[] args)
         {
             //Ora1();
+            //ExampleWhile();
+
+            //CastExample();
+
+
+            int[] sir = new int[] { (int)40.3, 3, 43, 4545, 564};
+
+            int[] sir2 = new int[6];
+
+            int[] sir3 = { (int)40.3, 3, 43, 4545, 564 };
+
+            int val = sir3[3];
+
+
+            int[] sir4 = new int[sir3.Length];
+
+
+            for (int i = sir3.Length - 1; i >= 0; i--)
+            {
+                sir4[sir4.Length - i - 1] = sir3[i];
+            }
+
+            Array.Reverse(sir3);
+
+            for (int i = 0; i < sir3.Length; i++)
+            {
+                Console.WriteLine(sir3[i]);
+            }
+            
+
 
             Console.ReadLine();
+        }
+
+        private static void CastExample()
+        {
+            int x = (int)40.5;
+
+            double y = x;
+        }
+
+        private static void ExampleWhile()
+        {
+            int[] sir = new int[] { 0, 3, 7, 8, 8, 8 };
+
+            int i = 0;
+
+            while (i < sir.Length)
+            {
+                Console.WriteLine(sir[i]);
+                i++;
+            }
         }
 
         private static void Ora1()
@@ -61,7 +111,7 @@ namespace Week02
             }
         }
 
-        private static void ExampleWhile()
+        private static void ExampleWhile2()
         {
             int nr = 0;
 
